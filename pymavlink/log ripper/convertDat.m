721d38b60baed8a5741de6bfbd651c913122a67e
tdw <cdr.awesomebeard@gmail.com>
2013-09-07 16:41:07 -0500

    Fixed convertDat
    convertDat now works again. Also, it stores the time associated with
    each message. It is not clear if this time corresponds to the message
    reception time; however, it can be used to order data vectors relative
    to one another, which is important.

--- pymavlink/log ripper/convertDat.m
+++ pymavlink/log ripper/convertDat.m
@@ -1,6 +1,7 @@
 % open a .bat file from mavlogripper.py, convert to .MAT file and .CSV file
 
 clear variables;
+EXCEL = 0;
 
 [filename,pathname] = uigetfile({'*.dat'});
 filename = [pathname, filename];
@@ -15,20 +16,30 @@ msg_len = [];
 msg_hdrs = cell(1,1);
 msg_count = [];
 data = {};
+time = {};%store the time vector for each message
+%parse each line
 while ~feof(fid)
     %parse until read semicolon thrice -> message ID
     line = fgets(fid);
-    [garbage, remain] = strtok(line,':');
+    %read the time - initial time is 1800 hrs = 1080 minutes
+    [garbage, remain] = strtok(line,' ');
+    remain(1) = [];%delete the spacebar character
+    hrs = str2num(remain(1:2)) - 18;
+    mins = str2num(remain(4:5));
+    secs = str2num(remain(7:8));
+    csecs = str2num(remain(10:11));
+    nowTime = 3600*hrs+60*mins+secs+.01*csecs;%current time in seconds
+    [garbage, remain] = strtok(remain,':');
     [garbage, remain] = strtok(remain,':');
     [garbage, remain] = strtok(remain,':');
     remain = remain(3:end);
     %get the message ID
     [msg_id,remain] = strtok(remain,' ');
-    if ~strcmp(msg_id,'PARAM_VALUE')
+    if ~strcmp(msg_id,{'PARAM_VALUE','STATUSTEXT'})
        %ignore param_value messages b/c they have a string in them and that
        %sucks
         remain = remain(3:end);%cut off the ' {' chars
-        if isempty( strmatch(msg_id,messages) )
+        if isempty( strmatch(msg_id,messages) )%if this is a new message, determine its size and initialize variables
             nummsgs = nummsgs + 1;
             msg_hdrs{nummsgs} = {};
             msg_count = [msg_count 1];
@@ -60,6 +71,8 @@ while ~feof(fid)
             end
             % initialize arrays to store this message data and store the first line
             data{nummsgs} = zeros(1e5,msg_len(end));
+            time{nummsgs} = zeros(1e5,1);
+            time{nummsgs}(1) = nowTime;
             data{nummsgs}(1,:) = line1;
         else
             %figure out which message this is
@@ -79,17 +92,26 @@ while ~feof(fid)
                     remain = remain(3:end);
                 end
                 data{msgint}(msg_count(msgint),i) = str2num(value);
+                time{msgint}(msg_count(msgint)) = nowTime;
             end
         end
     end
 end
 
-%export to .mat
-save([dotname, 'mat'],'data','messages','msg_count','msg_hdrs');
-%export to xls, multisheet
+%truncate the data vectors:
 for i = 1:nummsgs
-    xlswrite([dotname,'xls'],msg_hdrs{i},messages{i},'A1');
-    for j = 1:msg_count(i)
-        xlswrite([dotname,'xls'],data{i}(j,:),messages{i},['A' num2str(j+1)]);
+    data{i}(msg_count(i)+1:end,:) = [];
+    time{i}(msg_count(i)+1:end) = [];
+end
+
+%export to .mat
+save([dotname, 'mat'],'data','messages','msg_count','msg_hdrs','time');
+if EXCEL
+    %export to xls, multisheet
+    for i = 1:nummsgs
+        xlswrite([dotname,'xls'],msg_hdrs{i},messages{i},'A1');
+        for j = 1:msg_count(i)
+            xlswrite([dotname,'xls'],data{i}(j,:),messages{i},['A' num2str(j+1)]);
+        end
     end
 end
